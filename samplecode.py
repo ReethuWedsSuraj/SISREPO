f66c9e2fb2b824d7012aa88879daadde6363b70a
Reethu <129653022+ReethuWedsSuraj@users.noreply.github.com>
2024-11-22 13:37:19 +0530

    adding python file

---
+++ samplecode.py
@@ -0,0 +1,11 @@
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Define two numbers
+num1 = 10
+num2 = 20
+
+# Call the function and display the result
+result = add_numbers(num1, num2)
+print(f"The sum of {num1} and {num2} is {result}.")
